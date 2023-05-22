bbbee6ebe392183d7b8af349be1ab9bb9a158358
AhmedHamdy3 <ahmed195053@feng.bu.edu.eg>
2023-05-22 22:29:47 +0300

    separate BFS function from main function

--- main.cpp
+++ main.cpp
@@ -65,65 +65,61 @@ map<string, int> stoint;
 map<int, string> inttos;
 vector<vector<pair<int, Match>>> adj;
 vector<int> vis;
-
+queue<int> q;
 
 /* prototypes */
 
 int stringToInt(string s);
 int dateToInt(string s);
 bool srt(Team t1, Team t2);
+void BFS_date(int s, int choice, vector<Team> &team);
+void BFS_round(int s, int choice, vector<Team> &team);
 
 /* main function */
 
-// V (vertices) is the number of teams
-// E (Edges) is the number of played matches  
-
 int main(){
 
-    // O (E lg V)
     fstream data;
     string s, round, date, homeTeam, awayTeam, homeGoals, awayGoals, result; 
     data.open("epl_results.csv", ios::in);
     getline(data, s, ',');getline(data, s, ',');getline(data, s, ',');getline(data, s, ',');
-    getline(data, s, ',');getline(data, s, ',');getline(data, s, '\n');    // -> O(1)
-    while(getline(data, round, ',')){                               // -> O(E)
+    getline(data, s, ',');getline(data, s, ',');getline(data, s, '\n');
+    while(getline(data, round, ',')){
         getline(data, date, ',');getline(data, homeTeam, ',');getline(data, awayTeam, ',');
         getline(data, homeGoals, ',');getline(data, awayGoals, ',');getline(data, result, '\n');
         if(stoint.find(homeTeam) == stoint.end()){
-            nteam++;  // -> O(1)
-            stoint[homeTeam] = counter++;  // -> O(1)
-            inttos[counter] = homeTeam;    // -> O(1)
+            nteam++;
+            stoint[homeTeam] = counter++;
+            inttos[counter] = homeTeam;
         }
-        if(stoint.find(awayTeam) == stoint.end()){                  // -> O(lg V)
-            nteam++; // -> O(1)
-            stoint[awayTeam] = counter++; // -> O(1)
-            inttos[counter] = awayTeam; // -> O(1)
+        if(stoint.find(awayTeam) == stoint.end()){
+            nteam++;
+            stoint[awayTeam] = counter++;
+            inttos[counter] = awayTeam;
         }
     }
     data.close();
 
-    // O(E)
-    adj = vector<vector<pair<int, Match>>>(nteam);     // -> O(V)
-    vis = vector<int>(nteam, 0);                       // -> O(V)
+    adj = vector<vector<pair<int, Match>>>(nteam);
+    vis = vector<int>(nteam, 0);
 
     data.open("epl_results.csv", ios::in);
     getline(data, s, ',');getline(data, s, ',');getline(data, s, ',');getline(data, s, ',');
-    getline(data, s, ',');getline(data, s, ',');getline(data, s, '\n');    // O(1)
-    while(getline(data, round, ',')){  // O(E)
+    getline(data, s, ',');getline(data, s, ',');getline(data, s, '\n');
+    while(getline(data, round, ',')){
         getline(data, date, ',');getline(data, homeTeam, ',');getline(data, awayTeam, ',');
-        getline(data, homeGoals, ',');getline(data, awayGoals, ',');getline(data, result, '\n'); // O(1)
-        if(awayGoals == "-" ) continue; // O(1)
-        Match m (stringToInt(round), dateToInt(date), stringToInt(homeGoals), stringToInt(awayGoals), result); // O(1)
-        adj[stoint[homeTeam]].push_back({stoint[awayTeam],m}); // O(1)
+        getline(data, homeGoals, ',');getline(data, awayGoals, ',');getline(data, result, '\n');
+        if(awayGoals == "-" ) continue; 
+        Match m (stringToInt(round), dateToInt(date), stringToInt(homeGoals), stringToInt(awayGoals), result);
+        adj[stoint[homeTeam]].push_back({stoint[awayTeam],m});
     }
     data.close();
 
-    queue<int> q;
     while(true){
         vector<Team> teams;
         for(int i = 0; i <= nteam; ++i) vis[i] = 0;
-        for(auto el : inttos){          // O(V)
-            teams.push_back(Team(el.second));  // O(1)
+        for(auto el : inttos){
+            teams.push_back(Team(el.second));
         }
         int trio, choice; 
         cout << nl <<  "1 - Search by round " << nl << nl << "2 - search by date " << nl << nl << "3 - End The Program " << nl << nl ; 
@@ -132,47 +128,9 @@ int main(){
         if(trio == 1){
             cout << "Enter the round number : ";
             cin >> choice; 
-            
-            // O(V+E)
             for(int i = 1; i <= nteam; ++i){
-                if(!vis[i]){ // O(1)
-                    q.push(i);
-                    vis[i] = 1;
-                    while(!q.empty()){   // O(V)
-                        int u = q.front(); // O(1)
-                        q.pop(); // O(1)
-                        for(auto v : adj[u]){ // O(E)
-                            // # define v.second match 
-                            if(match.round <= choice){ // O(1)
-                                teams[u].goalsFor += match.homeGoals;
-                                teams[v.first].goalsAgainst += match.homeGoals;
-                                teams[u].goalsAgainst += match.awayGoals;
-                                teams[v.first].goalsFor += match.awayGoals;
-                                teams[u].matchPlayed++;
-                                teams[v.first].matchPlayed++;
-                                if(match.result == "H"){
-                                    teams[u].wins++;
-                                    teams[u].points +=3;
-                                    teams[v.first].loses ++;
-                                }
-                                else if(match.result == "A"){
-                                    teams[v.first].wins++;
-                                    teams[v.first].points +=3;
-                                    teams[u].loses ++;
-                                }
-                                else{
-                                    teams[u].drawns++;
-                                    teams[v.first].drawns++;
-                                    teams[u].points++;
-                                    teams[v.first].points++;
-                                }
-                            }
-                            if(!vis[v.first]){ // O(1)
-                                q.push(v.first); 
-                                vis[v.first] = 1;
-                            }
-                        }
-                    }
+                if(!vis[i]){
+                    BFS_round(i, choice, teams);
                 }
             }
         }
@@ -181,45 +139,9 @@ int main(){
             string da;
             cin >> da;
             choice = dateToInt(da);
-            // O(V+E)
             for(int i = 1; i <= nteam; ++i){
                 if(!vis[i]){
-                    q.push(i);
-                    vis[i] = 1;
-                    while(!q.empty()){
-                        int u = q.front();
-                        q.pop();
-                        for(auto v : adj[u]){
-                            if(match.date <= choice){
-                                teams[u].goalsFor += match.homeGoals;
-                                teams[v.first].goalsAgainst += match.homeGoals;
-                                teams[u].goalsAgainst += match.awayGoals;
-                                teams[v.first].goalsFor += match.awayGoals;
-                                teams[u].matchPlayed++;
-                                teams[v.first].matchPlayed++;
-                                if(match.result == "H"){
-                                    teams[u].wins++;
-                                    teams[u].points +=3;
-                                    teams[v.first].loses ++;
-                                }
-                                else if(match.result == "A"){
-                                    teams[v.first].wins++;
-                                    teams[v.first].points +=3;
-                                    teams[u].loses ++;
-                                }
-                                else{
-                                    teams[u].drawns++;
-                                    teams[v.first].drawns++;
-                                    teams[u].points++;
-                                    teams[v.first].points++;
-                                }
-                            }
-                            if(!vis[v.first]){
-                                q.push(v.first);
-                                vis[v.first] = 1;
-                            }
-                        }
-                    }
+                    BFS_date(i, choice, teams);
                 }
             }
         }
@@ -227,7 +149,7 @@ int main(){
             break;
         }
 
-        sort(teams.begin(), teams.end(), srt);  // O(V lg V)
+        sort(teams.begin(), teams.end(), srt);
 
         ofstream out("league standing.csv");
         cout << nl << nl;
@@ -247,9 +169,9 @@ int main(){
     
         out << "#" << ','<< "Team" << ',' << "M Played" << ',' << "Wins" << ',' <<
                 "Drawns" << ',' << "Loses" << ',' << "G For" << ',' << "G Against" << ',' <<
-                "G Diff" << ',' << "G Diff" << '\n';  // O(1)
+                "G Diff" << ',' << "G Diff" << '\n';
 
-        for(auto el : teams){   // O(V)
+        for(auto el : teams){
             cout<< left << setw(13) << cnt 
                 << left << setw(23) << el.teamName 
                 << left << setw(13) << el.matchPlayed 
@@ -277,7 +199,6 @@ int main(){
 
 /* Fuctions */
 
-// O(1)
 int stringToInt(string s){
     int res = 0;
     for(auto el: s)
@@ -287,16 +208,92 @@ int stringToInt(string s){
     return res;
 }
 
-// O(1)
 int dateToInt(string s){
     int year, month, day;
     std::sscanf(s.c_str(), "%d/%d/%d", &day, &month, &year) ;
     return 10000 * year + 100 * month + day;
 }
 
-// O(1)
 bool srt(Team t1, Team t2){
     if(t1.points != t2.points)return t1.points > t2.points;
-    else if(t1.goalsFor != t2.goalsFor) return t1.goalsFor - t1.goalsAgainst > t2.goalsFor - t2.goalsAgainst;
-    else return t1.goalsFor < t2.goalsFor;
+    else if(t1.goalsFor - t1.goalsAgainst != t2.goalsFor - t2.goalsAgainst) return t1.goalsFor - t1.goalsAgainst > t2.goalsFor - t2.goalsAgainst;
+    else return t1.goalsFor > t2.goalsFor;
+}
+
+void BFS_round(int s, int choice, vector<Team> &teams){
+    q.push(s);
+    vis[s] = 1;
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        for(auto v : adj[u]){
+            if(match.round <= choice){
+                teams[u].goalsFor += match.homeGoals;
+                teams[v.first].goalsAgainst += match.homeGoals;
+                teams[u].goalsAgainst += match.awayGoals;
+                teams[v.first].goalsFor += match.awayGoals;
+                teams[u].matchPlayed++;
+                teams[v.first].matchPlayed++;
+                if(match.result == "H"){
+                    teams[u].wins++;
+                    teams[u].points +=3;
+                    teams[v.first].loses ++;
+                }
+                else if(match.result == "A"){
+                    teams[v.first].wins++;
+                    teams[v.first].points +=3;
+                    teams[u].loses ++;
+                }
+                else{
+                    teams[u].drawns++;
+                    teams[v.first].drawns++;
+                    teams[u].points++;
+                    teams[v.first].points++;
+                }
+            }
+            if(!vis[v.first]){
+                q.push(v.first);
+                vis[v.first] = 1;
+            }
+        }
+    }
+}
+
+void BFS_date(int s, int choice, vector<Team> &teams){
+    q.push(s);
+    vis[s] = 1;
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        for(auto v : adj[u]){
+            if(match.date <= choice){
+                teams[u].goalsFor += match.homeGoals;
+                teams[v.first].goalsAgainst += match.homeGoals;
+                teams[u].goalsAgainst += match.awayGoals;
+                teams[v.first].goalsFor += match.awayGoals;
+                teams[u].matchPlayed++;
+                teams[v.first].matchPlayed++;
+                if(match.result == "H"){
+                    teams[u].wins++;
+                    teams[u].points +=3;
+                    teams[v.first].loses ++;
+                }
+                else if(match.result == "A"){
+                    teams[v.first].wins++;
+                    teams[v.first].points +=3;
+                    teams[u].loses ++;
+                }
+                else{
+                    teams[u].drawns++;
+                    teams[v.first].drawns++;
+                    teams[u].points++;
+                    teams[v.first].points++;
+                }
+            }
+            if(!vis[v.first]){
+                q.push(v.first);
+                vis[v.first] = 1;
+            }
+        }
+    }
 }
